6bd955ee0db6b9fadfc2bb4deb75b44608018767
sturniolo <br1.sturniolo@gmail.com>
2023-08-27 19:28:49 -0300

    first commit

---
+++ TP4/src/main/java/com/mycompany/tp4/TP4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp4;
+
+/**
+ *
+ * @author br1st
+ */
+public class TP4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
