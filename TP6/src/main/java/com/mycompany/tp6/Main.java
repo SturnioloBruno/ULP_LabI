3e5e737e8209a0b5a47fe7b8081fe9c28833e2c3
sturniolo <br1.sturniolo@gmail.com>
2023-09-05 11:42:37 -0300

    [FIX]vista proyectos

---
+++ TP6/src/main/java/com/mycompany/tp6/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp6;
+
+/**
+ *
+ * @author br1st
+ */
+public class Main {
+    public static void main(String[] args) {
+        MenuView mv = new MenuView();
+        mv.setVisible(true);
+    }
+}
